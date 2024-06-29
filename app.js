45eadda3e55efeb2b5c13706e7b44e7bb2d045c3
Caio Nishimura <caioagnishimura@gmail.com>
2024-06-29 11:42:20 -0300

    aprimorando o codigo

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 function sortear () {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
-
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
     let numero;
-    
     let sorteados = [];
+    let quantidadePossivel = ate - de + 1;
+    let validacoesOk = validacoes(quantidade, de, ate, quantidadePossivel);
 
-    for (let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio (de, ate);
-        sorteados.push(numero)
+
+    if (validacoesOk) {
+        for (let i = 0; i < quantidade; i++) {
+            do {
+                numero = obterNumeroAleatorio (de, ate);
+            } while (sorteados.includes(numero));
+            
+            sorteados.push(numero);
+        }
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+        alterarStatusBotao();
     }
+    
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function validacoes(quantidade, de, ate, quantidadePossivel) {
+    if(quantidade <= 0 ){
+        alert('Erro: insira uma quantidade válida!');
+        alterarStatusBotao();
+        return false;
+    } else if (de > ate) {
+        alert('Erro: o número mínimo é maior que o máximo!');
+        alterarStatusBotao();
+       return false;
+    } else if (quantidade > quantidadePossivel) {
+        alert('Erro: quantidade de elementos excedida!');
+        alterarStatusBotao();
+        return false;
+    }
+    return true;
+
+
+}   
